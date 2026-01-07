84a2a27d91c5ebfa5db4031994905b58691ff0cd
siddarami <msiddaramireddy@gmail.com>
2026-01-07 22:20:56 +0530

    adding file to test

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+
+person1=ramesh 
+person2=suresh 
+
+echo "hello world i am learning shell script"
+echo "$person1:Hello $person2 how are you"
+echo "$person2:Hello $person1, i am good how are you"
