b36cef52e91a22aea0e9e6321c43c31630b86509
siddarami <msiddaramireddy@gmail.com>
2026-01-07 22:02:52 +0530

    adding file to test

--- 01-helloworld.sh
+++ 01-helloworld.sh
@@ -1,3 +1,5 @@
 
 #!/bin/bash 
-echo"hello world i am learning shell script"
+echo "hello world i am learning shell script"
+echo "Ramesh:Hello suresh how are you"
+echo "suresh:Hello ramesh, i am good how are you"
