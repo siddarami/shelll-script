30aa46f1683b5c54c8bf322752f21494ccae4019
siddarami <msiddaramireddy@gmail.com>
2026-01-07 22:31:44 +0530

    adding file to test

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash 
+
+echo "enter the username "
+read username 
+echo "the username is : $username"
+
+echo "enter the password"
+read -s password 
+echo "the passsword is : $password"
