99ca21a6faab66f0f82d38edd7a87a146b376591
siddarami <msiddaramireddy@gmail.com>
2026-01-08 10:13:17 +0530

    adding file to test

--- 07-installgit.sh
+++ 07-installgit.sh
@@ -5,8 +5,16 @@ userid=$(id -u)
 
 if [ $userid -ne 0 ]
 then 
-    echo "you are not a root user,please run as root user"
+    echo "error::you are not a root user,please run as root user"
     exit 1
     fi 
 
 yum install git -y 
+
+if [ $? -ne 0 ]
+then 
+    echo "git installation failed"
+    exit 1
+else 
+    echo "git installed successfully"
+fi
