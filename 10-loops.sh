b5baf36f0619a1111adc1c5bafce099ee0f96980
siddarami <msiddaramireddy@gmail.com>
2026-01-10 19:11:03 +0530

    adding file to test

--- 10-loops.sh
+++ 10-loops.sh
@@ -1,9 +1,11 @@
 
 userid=$(id -u)
+R="\e[31m"
+]
 
 if [ $userid -ne 0 ];
 then 
-    echo -e "error:you are not a roor user,please run as root user"
+    echo -e " $R error:you are not a roor user,please run as root user"
     exit 1
 
 fi
