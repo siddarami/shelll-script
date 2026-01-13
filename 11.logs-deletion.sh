70803922b88095e2c053d0c737f40dbfd6a2bcb5
siddarami <msiddaramireddy@gmail.com>
2026-01-13 16:34:16 +0530

    adding file to test

--- 11.logs-deletion.sh
+++ 11.logs-deletion.sh
@@ -7,7 +7,7 @@ date=$(/usr/bin/date +%F)
 script_name=$(basename "$0")
 logfile=$log_path/$script_name-$date.log
 
-deleted_files=$(/usr/bin/find $log_path -name "*.log" -type f -mmin +1)
+deleted_files=$(/usr/bin/find $log_path -name "*.log" ! -name "$(basename "$logfile")" -type f -mmin +1)
 
 while read file
 do 
