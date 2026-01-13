b601c48d631bb9936d0d6275149dc853c9c80ff0
siddarami <msiddaramireddy@gmail.com>
2026-01-13 16:07:15 +0530

    adding file to test

--- 11.logs-deletion.sh
+++ 11.logs-deletion.sh
@@ -3,15 +3,15 @@
 #!/bin/bash 
 
 log_path="/home/centos/logs"
-date=$(date +%F)
-script_name=$0
-logfile=/home/centos/$script_name-$date.log
+date=$(/usr/bin/date +%F)
+script_name=$(basename "$0")
+logfile=$log_path/$script_name-$date.log
 
-deleted_files=$(find $log_path -name "*.log" -type f -mmin +1)
+deleted_files=$(/usr/bin/find $log_path -name "*.log" -type f -mmin +1)
 
 while read file
 do 
     echo "deleting file :$file" &>> $logfile
-    rm -rf $file  &>> $logfile
+    /bin/rm -rf $file  &>> $logfile
 
 done <<< "$deleted_files"  
