98b7d904f157050f8b8401c8fac727774a619799
siddarami <msiddaramireddy@gmail.com>
2026-01-14 13:53:48 +0530

    adding file to test

--- alertemail.sh
+++ alertemail.sh
@@ -8,7 +8,7 @@ threshold=10
 
 while IFS= read line 
 do 
-   usage=$( echo $line | awk '{print $6}' | cut -d '%' -f1)
+   usage=$( $line | awk '{print $6}' | cut -d '%' -f1)
    echo $usage
 
 done <<<$diskusage 
