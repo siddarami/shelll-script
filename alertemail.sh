ebe25724f23778550964e49b38fab8bf4c877137
siddarami <msiddaramireddy@gmail.com>
2026-01-14 14:00:24 +0530

    adding file to test

--- alertemail.sh
+++ alertemail.sh
@@ -10,6 +10,13 @@ while IFS= read line
 do 
    usage=$( echo $line | awk '{print $6}' | cut -d '%' -f1)
    echo $usage
+   partition=$( echo $line | awk '{print $1}')
+   if [ $usage -gt $threshold ];
+   then 
+         message="alert: high disk usage on partition $partition with usage at $usage"
+    fi
 
 done <<<$diskusage 
+
+echo "$message" 
  
